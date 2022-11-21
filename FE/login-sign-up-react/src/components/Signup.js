3249ed2726bcca74b985225df53569ab4ed6ddfe
fadillarahim <fadillarahimximipa1@gmail.com>
2022-11-21 22:11:07 +0700

    edit profile page

--- FE/login-sign-up-react/src/components/Signup.js
+++ FE/login-sign-up-react/src/components/Signup.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate} from 'react-router-dom';
 import axios from 'axios';
-// import useInput from '../hooks/useInput';
-// import FetchAPI from '../utils/API';
 import useToast from '../hooks/useToast';
 import logo from '../images/logo.png';
 import Input from './HidePasswordBtn';
--- FE/profile-react/src/App.js
+++ FE/profile-react/src/App.js
@@ -2,13 +2,14 @@
 import ShowProfile from "./components/show-profile";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EditProfile from "./components/Edit-profile";
+import Profile from "./components/profile";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/profile' element={<ShowProfile/>}/>
-      <Route path='/' element={<ShowProfile/>}/>
+      <Route path='/' element={<Profile/>}/>
       <Route path='/editprofile' element={<EditProfile/>}/>
     </Routes>
     </BrowserRouter>

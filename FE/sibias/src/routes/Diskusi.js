352715f310cb1c1eef9cf04a71e4dd3c2afd5675
Cahya Diantoni <cahyadiantoni@gmail.com>
2022-11-30 00:25:31 +0700

    update diskusi

--- FE/sibias/src/routes/Diskusi.js
+++ FE/sibias/src/routes/Diskusi.js
@@ -1,9 +1,33 @@
-import react from "react"
+import Hero from "./../components/Hero"
+import {React, useState} from "react"
+import TextField from "@mui/material/TextField";
+import List from "./../components/List"
+import "./css/Diskusi.css";
 
 function Diskusi(){
+    const [inputText, setInputText] = useState("");
+    let inputHandler = (e) => {
+        //convert input text to lower case
+        var lowerCase = e.target.value.toLowerCase();
+        setInputText(lowerCase);
+    };
     return (
-        <div className="diskusi">
-            <h1>Diskusi</h1>
+        <div>
+            <Hero />
+            <div className="main">
+                <h1>React Search</h1>
+                <div className="search">
+                    <button>Test</button>
+                    <TextField
+                    id="outlined-basic"
+                    onChange={inputHandler}
+                    variant="outlined"
+                    fullWidth
+                    label="Search"
+                    />
+                </div>
+                <List input={inputText} />
+            </div>
         </div>
     )
 }

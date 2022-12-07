350a20a5144fb29631f6ab947b0f5cc0f7626652
Cahya Diantoni <cahyadiantoni@gmail.com>
2022-12-07 12:31:35 +0700

    Tambah Diskusi

--- FE/sibias/src/routes/TambahDiskusi.jsx
+++ FE/sibias/src/routes/TambahDiskusi.jsx
@@ -1,10 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 import "./css/TambahDiskusi.css"
 import { BsFillArrowLeftCircleFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
 import Navbar from '../components/Navbar';
+import { Link, useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import useToast from '../hooks/useToast';
 
 function TambahDiskusi(){
+
+    const [showToast] = useToast();
+    const navigate = useNavigate();
+    const [judul, setJudul] = useState('');
+    const [msgJudul, setMsgJudul] = useState('');
+    const baseUrl = 'http://localhost:3000';
+
+    const addDiskusi = async (e) => {
+        e.preventDefault();
+        let isValid = true;
+
+        if (!judul) {
+            setMsgJudul('Judul is required'); 
+        }else{
+            setMsgJudul(''); 
+        }
+
+        if(isValid) {
+            try {
+              await axios.post(`${baseUrl}/diskusi`, {
+                judul_diskusi : judul,
+              });
+              showToast('Selamat Diskusi Berhasil Dibuat', 'success');
+              navigate("/diskusi");
+            } catch (error) {
+              if(error.response){
+                showToast(error.response.data.msg, 'fail');
+              }
+            }
+          }
+    }
+
     return (
         <>
         <Navbar/>
@@ -22,18 +56,20 @@ function TambahDiskusi(){
                         Buat Diskusi Baru
                     </div>
                 </div>
-                <form>
+                <form onSubmit={addDiskusi}>
                     <div className="mb-3">
                         <label className="form-label">Judul Diskusi</label>
-                        <input type="text" className="form-control" id="name-input" placeholder="Nama Anda"/>
+                        <input 
+                        className="form-control" 
+                        type="text" 
+                        placeholder="Judul Diskusi" 
+                        value={judul} 
+                        onChange={(e) => setJudul(e.target.value)} 
+                        />
                     </div>
-    
-                    <div className="mb-3">
-                        <label className="form-label">Keterangan</label>
-                        <textarea type="text" className="form-control" id="keterangan"></textarea>
-                    </div>
+                    <p className='warn-msg'>{msgJudul}</p> 
                     <div className="text-center update-profile-btn-container mt-5">
-                        <button type="" className="btn mb-3 update-profile-btn">Post</button>
+                        <button type="" className="btn mb-3 btn-buat-diskusi">Buat Diskusi</button>
                     </div>
                 </form>
                 </div>
